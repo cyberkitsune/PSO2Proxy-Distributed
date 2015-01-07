ab18f379ef606c7a06358c54efa10ce6fcbcc5f1
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2015-01-07 15:38:03 -0800

    List command

--- Master/Commands.py
+++ Master/Commands.py
@@ -19,3 +19,11 @@ def allexec(line):
     for server in ProxyServers:
         r.publish("proxy-server-%s" % server.name, json.dumps({'command': 'exec', 'input': line}))
         print("[PSO2PD] Sent command \"%s\" to %s" % (line, server.name))
+
+
+@CommandHandler("list")
+def list(line):
+    from ProxyServer import ProxyServers
+    for server in ProxyServers:
+        print("[List] Server \"%s\" - Usercount %i" % (server.name, server.users))
+    print("[List] %i proxy servers connected in total." % len(ProxyServers))
