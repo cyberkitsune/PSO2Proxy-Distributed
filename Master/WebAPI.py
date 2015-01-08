33d747d3cb4f1b3a37b67c632fd0ef8b90c679e3
Sean CyberKitsune McClenaghan <cyberkitsune09@gmail.com>
2015-01-07 18:28:06 -0800

    add rcon

--- Master/WebAPI.py
+++ Master/WebAPI.py
@@ -1,5 +1,6 @@
 import calendar
 import datetime
+import traceback
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.web import server
@@ -13,7 +14,7 @@ import os
 upStart = calendar.timegm(datetime.datetime.utcnow().utctimetuple())
 peakPlayers = 0
 
-web_config = YAMLConfig("web.cfg.yaml", {"hostname": "example.com", "port": 8080, "servername": "PSO2Proxy Public Network"}, True)
+web_config = YAMLConfig("web.cfg.yaml", {"hostname": "example.com", "port": 8080, "servername": "PSO2Proxy Public Network", "rconpass": "changeme"}, True)
 
 hostName = web_config['hostname']
 port = web_config['port']
@@ -65,6 +66,30 @@ class WebAPI(Resource):
         if name == '':
             return self
         return Resource.getChild(self, name, request)
+
+
+class WEBRcon(Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        from Commands import Commands
+        request.setHeader('content-type', "application/json")
+        if 'key' not in request.args or request.args['key'][0] != web_config['rconpass']:
+            return json.dumps({'success': False, 'reason': "Your RCON key is invalid!"})
+        else:
+            if 'command' not in request.args:
+                return json.dumps({'success': False, 'reason': "Command not specified."})
+            else:
+                try:
+                    if request.args['command'][0] in Commands:
+                        Commands[request.args['command']]()
+                        return json.dumps({'success': True, 'output': "Command sent."})
+                    else:
+                        return json.dumps({'success': False, 'reason': "Command not found."})
+                except:
+                    e = traceback.format_exc()
+                    return json.dumps({'success': False, 'reason': "Error executing command\n%s" % e})
+
 
 
 def setup_web():
